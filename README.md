b1b50efc08dbba982af9981f5da115aa0761fd9c
Bryan Coulter <bryandcoulter@gmail.com>
2017-06-23 09:24:16 -0500

    Adds webpack build (#1)
    * initial update to use Webpack and code splitting
    
    * remove dist and update gitignore
    
    * cleaned up webpack config, rename view bundle and update readme
    
    * update package.json to read master branch of component-loader

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Polymer 2 Starter Kit built with Webpack
+# Polymer 2 Starter Kit with Webpack
+## Overview
+Proof of concept in using webpack to build the Polymer 2 Starter Kit.
+
+## Get Started
+Install:
+```
+bower install
+npm install OR yarn
+```
+Build the App:
+```
+webpack
+```
+Serve the App:
+```
+polymer serve
+```
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+'use strict';
+/* global __dirname module require*/
+/* eslint comma-dangle: ["error", "never"] */
+const path = require('path');
+
+module.exports = {
+  entry: './src/my-app.html',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, './dist'),
+    publicPath: 'dist/'
+  },
+  resolve: {
+    modules: ['node_modules', 'bower_components'],
+    descriptionFiles: ['package.json']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'component-loader'
+          }
+        ]
+      }
+    ]
+  }
+};
